426dfa3234232e2e8c0ef968e1eb9b4f86124a19
scarjo-ai <scarjo@scarlett.ai>
2025-06-28 18:46:16 +0100

    feat(emissions): add module registration message to proto

--- x/emissions/keeper/msg_server.go
+++ x/emissions/keeper/msg_server.go
@@ -1,6 +1,8 @@
 package keeper
 
 import (
+	"context"
+
 	"scarlett-core/x/emissions/types"
 )
 
@@ -15,3 +17,9 @@ func NewMsgServerImpl(keeper Keeper) types.MsgServer {
 }
 
 var _ types.MsgServer = msgServer{}
+
+// RegisterModule - temporary stub implementation (will be implemented in Step 1.3)
+func (m msgServer) RegisterModule(ctx context.Context, req *types.MsgRegisterModule) (*types.MsgRegisterModuleResponse, error) {
+	// TODO: Implement in Step 1.3
+	return &types.MsgRegisterModuleResponse{}, nil
+}
